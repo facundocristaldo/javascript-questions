8c13cec4c3b46bdddfdb8b939f03226f86102624
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-05-04 20:44:27 -0300

    favicon link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Head from "next/head";
+const prefix = "/javascript-questions"
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <link rel="shortcut icon" href={`${prefix}/favicon.ico`} />
+      <title>Javascript Questions</title>
+    </Head>
     <div>A starting point</div>
+    </>
   )
 }
